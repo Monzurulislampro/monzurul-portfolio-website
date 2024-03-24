92e489b768335987feac6d2a2b836d93ef600984
Monzurulislampro <mounzrul@gmail.com>
2024-03-24 12:26:11 +0600

    icons width fix

--- src/Components/ExperienceWith.jsx
+++ src/Components/ExperienceWith.jsx
@@ -1,8 +1,8 @@
 const ExperienceWith = () => {
   return (
-    <div className="grid grid-cols-1 mx-auto md:gap-4 max-w-[75%] mt-10">
+    <div className="grid grid-cols-1 mx-auto gap-4 max-w-[75%] mt-10">
       <div className="text-2xl mx-auto">EXPERIENCE WITH</div>
-      <div className="flex mx-auto md:gap-4">
+      <div className="flex mx-auto md:gap-4 w-8 items-center justify-center">
         <img src="/assets/icons8-html.svg" alt="" />
         <img src="/assets/icons8-css.svg" alt="" />
         <img src="/assets/icons8-javascript.svg" alt="" />
